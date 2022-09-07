a8159333c48746b8830950145c8b88574419a656
React Dev <113049933+ItsKemboi@users.noreply.github.com>
2022-09-07 20:35:53 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Hello</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+        />
+      </form>
+    );
+  }
+}
+ReactDOM.render(<MyForm />, document.getElementById('root'));
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
